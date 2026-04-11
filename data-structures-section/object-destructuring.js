94ad5ef44acc9f60e743a822f3368ea4efbc5eff
Bill Walker Fosuhene <billwalker.bwf@gmail.com>
2026-04-11 06:47:58 +0000

    correct object file

--- data-structures-section/object-destructuring.js
+++ data-structures-section/object-destructuring.js
@@ -89,3 +89,38 @@ restaurant.orderDelivery({
 //   address: 'kwadaso-Estate, Ak-225-336-1',
 //   mainIndex: 2,
 // });
+
+// NOTE CORRECTIONS
+const meal = {
+  eatryName: "KFC",
+  firstFood: "Tea n Bread",
+  secondFood: "Rice n Stew",
+  thirdFood: "Banku n Okro stew",
+  dishes: ["kelewele", "chips", "assorted foods"],
+  userOrder: function ({ firstMeal, secondMeal, time, address }) {
+    return `I visit KFC at ${time} in the morning for their ${this.dishes[firstMeal]} and ${this.dishes[secondMeal]} at ${address}`;
+  },
+  userOrders: function (eatryName, firstFood) {
+    return `I go to ${eatryName} for their ${firstFood}`;
+  },
+};
+
+console.log(
+  meal.userOrder({
+    time: "9:00",
+    firstMeal: 1,
+    secondMeal: 0,
+    address: "Ahodwo",
+  }),
+);
+
+const { eatryName: jointName, firstFood: order1 } = meal;
+console.log(meal.userOrders(jointName, order1));
+
+// Renaming meal
+const { firstFood: Breakfast, secondFood: lunch, thirdFood: supper } = meal;
+console.log(Breakfast, lunch, supper); // Tea n Bread, Rice n Stew, Banku n Okro stew
+
+const { xi: x, yi: y } = { xi: 10, yi: 20 };
+console.log(x, y);
+// console.log(xi, yi);
