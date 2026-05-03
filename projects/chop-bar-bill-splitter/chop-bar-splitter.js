cf92b643784ef4d048a13d3ba7b57cc515488335
Bill Walker Fosuhene <billwalker.bwf@gmail.com>
2026-05-03 03:31:53 +0000

    Add data layer for chop bar bill splitter

---
+++ projects/chop-bar-bill-splitter/chop-bar-splitter.js
@@ -0,0 +1,58 @@
+"use strict";
+// ===== MENU =====
+// your menu items as an array of objects
+const menu = [
+  {
+    name: "waakye",
+    price: 30,
+    category: "main",
+  },
+  {
+    name: "rice & stew",
+    price: 25,
+    category: "main",
+  },
+  {
+    name: "banku & okro stew",
+    price: 40,
+    category: "main",
+  },
+  {
+    name: "Sobolo",
+    price: 15,
+    category: "drink",
+  },
+  {
+    name: "Malta Gunines",
+    price: 10,
+    category: "drink",
+  },
+  {
+    name: "tilapia",
+    price: 50,
+    category: "protein",
+  },
+];
+
+// ===== ORDERS =====
+// your orders array
+const order = [
+  {
+    customer: "Bill",
+    tableNumber: 1,
+    items: [],
+    total: 0,
+  },
+  {
+    customer: "Oscar",
+    tableNumber: 2,
+    items: [],
+    total: 0,
+  },
+];
+
+// ===== CUSTOMERS =====
+const customers = [
+  { name: "Bill", tableNumber: 1 },
+  { name: "Oscar", tableNumber: 2 },
+];
