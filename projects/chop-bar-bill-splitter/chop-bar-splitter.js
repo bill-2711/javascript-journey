476ae120f728efb5a3a2fdf637a612089e883cf5
Bill Walker Fosuhene <billwalker.bwf@gmail.com>
2026-05-25 19:32:57 +0000

    testing from a while

--- projects/chop-bar-bill-splitter/chop-bar-splitter.js
+++ projects/chop-bar-bill-splitter/chop-bar-splitter.js
@@ -61,28 +61,36 @@ const customers = [
   { name: "Bill", tableNumber: 1 },
   { name: "Oscar", tableNumber: 2 },
 ];
+let userInput = Number(prompt("Select user between 0 or 1"));
+let userMenu = Number(prompt("Select your menu order from the list [0 - 6]"));
 
+let userBill = 0;
 // DISPLAY all menu items
 for (const food of menu) {
   const { name, price, category } = food;
   console.log(`[${category}] ${name} - GH₵${price ?? "Not priced yet"}`);
+  userBill += menu[userMenu]?.price;
 }
-
-let userInput = Number(prompt("Select user between 0 or 1"));
 // Selecting current customer — smart use of optional chaining
 const currentCustomer = order?.[userInput];
 
 // Welcoming the customer — clean template literal
 console.log(`Welcome ${currentCustomer.customer}`);
 
-let userMenu = Number(prompt("Select your menu order from the list [0 - 6]"));
 // Selecting menu item — correct array access
 const userMenuSelect = menu[userMenu];
+// for (const [key, value] of Object.entries(userMenuSelect)) {
+//   console.log(key, value);
+// }
 
 // Adding to order — correct use of push
 order[userInput]?.items.push(userMenuSelect);
 console.log(order[userInput]?.items);
 
+// User confirmation message
 console.log(
   `Mr.${currentCustomer.customer}, you selected our ${menu[userMenu]?.category} ${menu[userMenu]?.name} and it's GH₵${menu[userMenu]?.price}.`,
 );
+
+// User total bill
+console.log(`Your total bill: ${userBill}`);
